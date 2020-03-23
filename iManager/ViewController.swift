e031334cda03f9ba7918cdb29f4db21d963436ad
肖仲文 <zhongwenxiao@hotmail.com>
2020-03-23 21:47:56 +0800

    first commit

---
+++ iManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iManager
+//
+//  Created by 肖仲文 on 2020/3/23.
+//  Copyright © 2020 肖仲文. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iManagerTests/iManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  iManagerTests.swift
+//  iManagerTests
+//
+//  Created by 肖仲文 on 2020/3/23.
+//  Copyright © 2020 肖仲文. All rights reserved.
+//
+
+import XCTest
+@testable import iManager
+
+class iManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
